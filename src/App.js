5b15c694356bf2c1824a7d97b318fa99179bcd2a
Jean Luc Abayo <luc.bayo@gmail.com>
2018-11-15 11:58:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import "./App.css";
-import Input from "./components/TextInput/TextInput";
-import CustomButton from "./components/Buttons/CustomButton";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: "",
-			password: "",
-			isLoading: false
-		};
-	}
-	onChangeTextInput = (e, key) => {
-		this.setState({
-			[key]: e.target.value
-		});
-	};
-
-	onSend = () => {
-		console.log(this.state);
-	};
 	render() {
 		return (
 			<div className="App">
-				<h2>SheCanCode</h2>
-				<div className="login-form">
-					<Input
-						label="Email"
-						type="email"
-						placeholder="me@example.com"
-						value={this.state.email}
-						onChange={e => this.onChangeTextInput(e, "email")}
-					/>
-					<Input
-						label="Pasword"
-						type="password"
-						value={this.state.password}
-						onChange={e => this.onChangeTextInput(e, "password")}
-					/>
-					<CustomButton title="Send" onClick={() => this.onSend()} />
-				</div>
+				<Router>
+					<div>
+						<Route exact path="/" component={Login}/>
+						<Route exact path="/signup" component={Signup}/>
+					</div>
+				</Router>
 			</div>
 		);
 	}
